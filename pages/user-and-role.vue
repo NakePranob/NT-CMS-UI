b35ab843110ca2414d91baa584e14ef511fcef82
NakePranob <pranob.onphaeng@gmail.com>
2024-10-31 00:44:57 +0700

    add filter future

--- pages/user-and-role.vue
+++ pages/user-and-role.vue
@@ -16,23 +16,23 @@
                 Add New Post
             </UButton>
         </div>
-        <div class="flex justify-between">
+        <div class="flex flex-col gap-2 md:flex-row justify-between">
             <div class="flex gap-2">
-                <USelectMenu v-model="bulkSelected" :options="bulk" multiple placeholder="Bulk actions"/>
-                <UButton label="Apply" class="text-black"/>
-                <USelectMenu v-model="roleSelected" :options="role" multiple placeholder="Change role to.."/>
-                <UButton label="Filter" class="text-black"/>
+                <USelectMenu v-model="bulkSelected" :options="bulk" multiple placeholder="Bulk actions" />
+                <UButton label="Apply" class="text-black" />
+                <USelectMenu v-model="roleSelected" :options="role" multiple placeholder="Change role to.." />
+                <UButton label="Filter" class="text-black" @click="filteredRows" />
             </div>
             <div class="flex gap-2">
-                <UInput icon="i-heroicons-magnifying-glass" placeholder="Search..."/>
-                <UButton label="Search" class="text-black"/>
+                <UInput v-model="q" icon="i-heroicons-magnifying-glass" placeholder="Search..." />
+                <UButton label="Search" class="text-black" @click="filteredRows" />
             </div>
         </div>
         <div class="flex justify-end opacity-60 py-2 pe-4">
-            10 item
+            {{ pageCount }} item
         </div>
-        <div class="border shadow-sm rounded-md">
-            <UTable v-model="selected" :rows="people" :columns="columns" @select="select">
+        <div class="border dark:border-gray-700 shadow-sm rounded-md">
+            <UTable v-model="selected" :rows="rows" :columns="columns" @select="select">
                 <template #username-data="{ row }">
                     <div class="flex items-center gap-3 -my-2">
                         <div class="w-14 h-14 rounded bg-slate-100 flex justify-center items-center">
@@ -44,11 +44,11 @@
                     </div>
                 </template>
             </UTable>
-            <div class="p-3 border-t">
-                <UPagination v-model="page" :page-count="pageCount" :total="people.length" :ui="{
+            <div class="p-3 border-t dark:border-gray-700">
+                <UPagination v-model="page" :page-count="pageCount" :total="peopleCount" :ui="{
                     wrapper: 'flex items-center gap-1',
                     rounded: '!rounded-full min-w-[32px] justify-center ring-0',
-                }"/>
+                }" />
             </div>
         </div>
     </div>
@@ -72,28 +72,206 @@ const columns = [{
     key: 'post',
     label: 'Post'
 }]
-const people = [{
-    username: 'admin',
-    name: 'Lindsay Walton',
-    email: 'lindsay.walton@example.com',
-    role: 'editer',
-    post: 7,
-    image: '/img/logo.png'
-}, {
-    username: 'user',
-    name: 'Courtney Henry',
-    email: 'courtney.henry@example.com',
-    role: 'Admin',
-    post: 4,
-    image: ''
-}]
+const people = [
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 0,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 1,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 2,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 3,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 4,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 5,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 6,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 7,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 0,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 1,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 2,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 3,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 4,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 5,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 6,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 7,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 0,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 1,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 2,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 3,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 4,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 5,
+        image: ''
+    },
+    {
+        username: 'admin',
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Editer',
+        post: 6,
+        image: '/img/logo.png'
+    }, 
+    {
+        username: 'user',
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Admin',
+        post: 7,
+        image: ''
+    },
+]
 
 const page = ref(1)
-const pageCount = 1
+const peopleCount = ref(people.length)
+const pageCount = 4
 
-const rows = computed(() => {
-  return people.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
+const rows = ref([])
 
 function select(row) {
     const index = selected.value.findIndex(item => item.id === row.id)
@@ -112,4 +290,34 @@ const bulkSelected = ref([])
 const role = ['Admin', 'Menager', 'Editer']
 const roleSelected = ref([])
 
+const q = ref('')
+
+onMounted(() => {
+    filteredRows()
+})
+
+watch([page], () => {
+    filteredRows()
+})
+
+const filteredRows = () => {
+    let filtered = people;
+
+    // ถ้ามี roleSelected ให้กรองตาม role ก่อน
+    if (roleSelected.value.length > 0) {
+        filtered = filtered.filter(person => roleSelected.value.includes(person.role));
+    }
+
+    // ถ้ามี q ให้กรองตามคำค้นหาด้วย
+    if (q.value) {
+        filtered = filtered.filter(person =>
+            Object.values(person).some(value =>
+                String(value).toLowerCase().includes(q.value.toLowerCase())
+            )
+        );
+    }
+    peopleCount.value = filtered.length;
+    // ตัดแบ่งข้อมูลตามหน้า (pagination)
+    rows.value = filtered.slice((page.value - 1) * pageCount, page.value * pageCount);
+};
 </script>
